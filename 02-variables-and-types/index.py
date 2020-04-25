6132aeb68ab9c8273d50078ef70de8efd6e0aa47
Anthony Tamayo Ortega <anthonytamayoortega@Anthonys-iMac.local>
2020-04-25 13:17:17 -0500

    i add variables , types, interpolation and concatenation

--- 02-variables-and-types/index.py
+++ 02-variables-and-types/index.py
@@ -1,12 +1,23 @@
 
 """
-Variables
+Variables, concatenación, interpolación y castear
 """
 
+# Variables
 text = "Master en python"
 age = "Edad: "
 num = 26
+name = "Anthony"
+lastname = "Tamayo Ortega"
+web = ""
 
 print(text)
-print(age)
-print(num)
+# Concatenación, interpolación y castear de entero a cadena de texto
+print(f"{age} "+str(num)+" años")
+
+name = "Anthony"
+lastname = "Tamayo Ortega"
+web = "anthonyjobs.co"
+
+# Otra forma de concatenar
+print("Hola me llamo {} {} y mi web es: {}".format(name,lastname,web))
