777923e8ceb9c01ecc0264fce964541e89108424
Anthony Tamayo Ortega <anthonytamayoortega@Anthonys-iMac.local>
2020-04-25 00:37:21 -0500

    i add hello world and variables with Python

---
+++ 01-helloworld/index.py
@@ -0,0 +1,7 @@
+
+# Esto es un hola mundo y así se colocan los comentarios
+print("Hello world i'm Anthony Tamayo Ortega Web Developer")
+
+"""
+Esto es un comentario multilinea
+"""
---
+++ 02-variables-and-types/index.py
@@ -0,0 +1,12 @@
+
+"""
+Variables
+"""
+
+text = "Master en python"
+age = "Edad: "
+num = 26
+
+print(text)
+print(age)
+print(num)
