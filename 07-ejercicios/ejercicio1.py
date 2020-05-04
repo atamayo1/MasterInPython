b2c599a81a316e73540d3e84cb3b94df575f3829
Anthony Tamayo Ortega <anthonytamayoortega@Anthonys-iMac.local>
2020-05-03 21:47:18 -0500

    i did the exercise one

---
+++ 07-ejercicios/ejercicio1.py
@@ -0,0 +1,16 @@
+
+"""
+
+Ejercicio 1:
+     - Crear variables una "pais" y otra "continente"
+     - Mostrar su valor por pantalla (imprimir)
+     - Poner un comentario diciendo el tipo de dato
+"""
+
+pais = input("Ingrese el pais: ")
+continente = input("Ingrese el continente: ")
+year = int(input("Ingrese el año actual: "))
+
+print(f"El tipo de dato del país {pais} es: {type(pais)}")
+print(f"El tipo de dato del continente {continente} es: {type(continente)}")
+print(f"El tipo de dato del año {str(year)} es: {type(year)}")
